4861aeac8b932658bfde99d74ace6fe767710c9a
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-07-08 19:49:09 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-API_KEY = "V5rCwrOm0FIwhe21K1OWmA2ZS2X1ku7UJ217fsGrDdM2WMGTmihEglPBOwvIbLEp"
-API_SECRET = "dHgqzkMJCLTTHxsFQh6oNzDTf3mlQeWw1bV9GiLHS267j7mIk3CtfLKJzu0zCR0O"
-BASE_URL = "https://testnet.binance.vision"
-
